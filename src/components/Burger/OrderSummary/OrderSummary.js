2c087ba3715ceadbf7e694a1060eaba49d06e038
Assaf Cohen <assafcohen4@gmail.com>
2018-07-20 13:06:36 +0300

    sidedrawer backdrop layout

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Auxiliary from '../../../hoc/Auxiliary'
+import Button from '../../UI/Button/Button'
+
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
             return (
-                <li>
+                <li key={igKey}>
                     <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
                 </li>
             )
@@ -17,7 +19,10 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINTUE</Button>
         </Auxiliary>
     )
 };
