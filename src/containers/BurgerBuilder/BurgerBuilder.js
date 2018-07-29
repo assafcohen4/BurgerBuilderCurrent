c3e6d107d260dd4b3eb19163f3bc9dd8627aff96
Assaf Cohen <assafcohen4@gmail.com>
2018-07-29 19:09:18 +0300

    making contactForm dynamic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,30 +89,13 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        /* //alert('you continue');
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max',
-                addres: {
-                    street: 'testStreet',
-                    zipCode: '4135',
-                    country: 'Germany'
-                },
-                email: "test@yummy.com"
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => this.setState({ loading: false, purchasing: false }))
-            .catch(error => this.setState({ loading: false, purchasing: false }));
- */     
+       
         const queryParams=[];
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+
+        queryParams.push('price=' + this.state.totalPrice)
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
